8ff7e93d3bfbe8d44222b0922ad79b7656b7cebe
goku2130 <gokul6625@gmail.com>
2020-08-07 16:54:47 +0200

    Added main

---
+++ main.py
@@ -0,0 +1,32 @@
+import gym
+import time
+from DQN import DQNModel,DQNAgent
+
+def test_model():
+    env = gym.make('CartPole-v0')
+    print('num_actions: ', env.action_space.n)
+    model = DQNModel(env.action_space.n, 'DQN')
+
+    obs = env.reset()
+    print('obs_shape: ', obs.shape)
+
+    # tensorflow 2.0: no feed_dict or tf.Session() needed at all
+    best_action, q_values = model.action_value(obs[None])
+    print('res of test model: ', best_action, q_values)  # 0 [ 0.00896799 -0.02111824]
+
+if __name__ == '__main__':
+    test_model()
+
+    env = gym.make("CartPole-v0")
+    num_actions = env.action_space.n
+    model = DQNModel(num_actions, 'DQN1')
+    target_model = DQNModel(num_actions, 'DQN2')
+    agent = DQNAgent(model, target_model,  env)
+    # test before
+    rewards_sum = agent.evaluation(env)
+    print("Before Training: %d out of 200" % rewards_sum) # 9 out of 200
+
+    agent.train()
+    # test after
+    rewards_sum = agent.evaluation(env)
+    print("After Training: %d out of 200" % rewards_sum) # 200 out of 200
